47e5d210881395ba082c42cec24745c55a6d61a1
Debian <admin@ip-172-31-21-105.ap-south-1.compute.internal>
2019-03-20 11:41:36 +0000

    Tomcat restart added in deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,4 @@ set -e
 sudo rm -rf /var/lib/tomcat8/webapps/book
 sudo mkdir /var/lib/tomcat8/webapps/book
 sudo unzip target/bookstore-0.1.war -d /var/lib/tomcat8/webapps/book
+sudo systemctl restart tomcat8
